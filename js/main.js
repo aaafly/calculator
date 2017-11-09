a69a31e1cde2ec75d1ef1413a2b2bd807feda767
aaafly <aaaflyvw@gmail.com>
2017-11-09 12:11:40 -0500

    Bugfix : start from new on reuse the last answer when starting typing again after an answer.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
-var input = '';
+var input = 0;
+var showingAnswer = false;
 // input = "111-1+1/1+(22/2-222(3*3(4+4)))+(11-99/1+(2+2+(3+3(4+4+4)+3)))";
 var indicatorElem = $("#result");
 
 $("#reset").on('click', function () {
   input = 0;
-  indicatorElem.html(input);
+  indicatorElem.html(0);
+  showingAnswer = false;
 });
 
 $(".entry").on('click', function () {
-  if (input === 'Err') {
-    input = $(this).html()
+  var typed = $(this).html();
+
+  if (
+    input === 'Err' ||
+    (showingAnswer && (typed.match(/[0-9]/) || []).length)
+  ) {
+    input = typed;
   } else {
-    input += $(this).html();
+    input += typed;
   }
 
   indicatorElem.html(input);
+  showingAnswer = false;
 });
 
 $("#equals").on('click', function () {
   var stringEquation = new StringEquation(input);
   input = stringEquation.answer
   indicatorElem.html(input);
+  showingAnswer = true;
 });
