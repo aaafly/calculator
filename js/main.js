b31d385e4d2587bc35fdc60a7bf31e03150e9241
aaafly <aaaflyvw@gmail.com>
2017-11-09 11:54:51 -0500

    Implemented the reset button.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ var input = '';
 // input = "111-1+1/1+(22/2-222(3*3(4+4)))+(11-99/1+(2+2+(3+3(4+4+4)+3)))";
 var indicatorElem = $("#result");
 
+$("#reset").on('click', function () {
+  input = 0;
+  indicatorElem.html(input);
+});
+
 $(".entry").on('click', function () {
   input += $(this).html();
   indicatorElem.html(input);
